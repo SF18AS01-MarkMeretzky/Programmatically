3a9ebb9adde9d6eeda969fa46ab8f64c22474774
Instructor <instructor@instructoriMac-8170.local>
2018-12-27 12:20:46 -0500

    I added a UITextField.

--- Programmatically/ViewController.swift
+++ Programmatically/ViewController.swift
@@ -10,20 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let label: UILabel = UILabel();
+    
     override func viewDidLoad() {
         super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
-        let label: UILabel = UILabel();
+        let s: String = """
+        Clint Eastwood said \"Make my day\"
+        in Sudden Impact.
+        """;
+        print(s);
+        
+        
+        
         label.backgroundColor = .yellow;
         label.numberOfLines = 0;   //0 permits any number of lines
         
-        label.text = """
-        Close one eye.
-        Stare at the black dot.
-        Do not blink.
-        """;
+        let greeting: String = "Hello, world!"
+
+        if greeting.hasPrefix("Hello".lowercased()) {
+            label.text = "It begins with Hello";
+        } else {
+            label.text = "It doesn't begin with Hello";
+        }
         
-        label.frame.origin = CGPoint(x: 100, y: 100);  //frame is a CGRect.
+        
+        label.frame.origin = CGPoint(x: 50, y: 100);  //frame is a CGRect.
         label.frame.size = label.intrinsicContentSize; //needed if not using constraints
         print("label.frame = \(label.frame)");         //(100.0, 100.0, 169.0, 61.0)
         print(view.subviews.count);
@@ -31,6 +43,16 @@ class ViewController: UIViewController {
         print(view.subviews.count);
     }
 
-
+    @IBAction func returnButtonPressed(_ sender: UITextField) {
+        
+        sender.resignFirstResponder();
+        
+        if sender.text!.lowercased().hasPrefix("hello") {
+            label.text = "Friendly!"
+        } else {
+            label.text = "Unfriendly!"
+        }
+    }
+    
 }
 
